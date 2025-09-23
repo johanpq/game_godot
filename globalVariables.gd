5e91c5e4c20e7998f2863bcf3ef26c0944fa8298
johanpq <johanp.queiroz@gmail.com>
2025-09-23 19:00:55 -0300

    feat: added dialog

--- globalVariables.gd
+++ globalVariables.gd
@@ -1,6 +1,7 @@
 # Global.gd
 extends Node
 
+var quest_open = false
 var npc1_locked = false # trava quando completado
 var npc2_locked = false      # trava se missao 1 nao for completada
 var mural1_completed = false   # missão 1 completada
